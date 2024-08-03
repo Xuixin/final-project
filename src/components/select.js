1a7376223b4b7fa5be5b62e6fa9893091cc9f1cc
Xuixin <146445237+Xuixin@users.noreply.github.com>
2024-08-04 00:33:03 +0700

    fixerror

--- src/components/select.js
+++ src/components/select.js
@@ -1,47 +1,50 @@
 'use client'
-import * as React from "react"
+import * as React from 'react'
 
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { useState, useEffect } from "react"
-import Link from "next/link"
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function SelectPath({ path }) {
-    const [pathselect, setPathselect] = useState('')
+  const [pathselect, setPathselect] = useState('')
 
-    useEffect(() => {
-        // Log the path to check when and why useEffect is triggered
-        console.log("Current path:", path); 
-        
-        switch (path) {
-            case '/admin':
-                setPathselect('menu');
-                break;
-            case '/admin/menutype':
-                setPathselect('Menutype');    
-                break;
-            default:
-                setPathselect(''); // Reset if path doesn't match
-        }
-    }, [path]); // Only run this effect when 'path' changes
+  useEffect(() => {
+    // Log the path to check when and why useEffect is triggered
+    console.log('Current path:', path)
+    switch (path) {
+      case '/admin':
+        setPathselect('menu')
+        break
+      case '/admin/menutype':
+        setPathselect('Menutype')
+        break
+      default:
+        setPathselect('') // Reset if path doesn't match
+    }
+  }, []) // Only run this effect when 'path' changes
 
-    return (
-        <Select>
-            <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder={pathselect} />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    <Link href={'/admin'}><SelectItem>menu</SelectItem></Link>
-                    <Link href={'/admin/menutype'}><SelectItem>menutype</SelectItem></Link>
-                </SelectGroup>
-            </SelectContent>
-        </Select>
-    )
+  return (
+    <Select>
+      <SelectTrigger className='w-[180px]'>
+        <SelectValue placeholder={pathselect} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <Link href={'/admin'}>
+            <SelectItem>menu</SelectItem>
+          </Link>
+          <Link href={'/admin/menutype'}>
+            <SelectItem>menutype</SelectItem>
+          </Link>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
 }
