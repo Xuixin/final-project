c3086c158228fd2bb3fc21473350e46c907441ba
Xuixin <146445237+Xuixin@users.noreply.github.com>
2024-08-16 23:38:03 +0700

    add padding in customer layout

--- src/app/(customer)/home/page.js
+++ src/app/(customer)/home/page.js
@@ -1,8 +1,3 @@
-export default function Home(){
-    return (
-        <div>
-            <h1>Welcome to Next.js</h1>
-            <p>This is your first Next.js app.</p>
-        </div>
-    )
+export default function Home() {
+  return <div>hi</div>
 }
